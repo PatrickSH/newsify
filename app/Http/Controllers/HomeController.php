3126337629c134da5267743bd8e2c59fdc8d8b3d
Patrick Hansen <patrick.hansen@hotmail.com>
2019-02-02 21:00:46 +0000

    Started main view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ use App\Integrations\TV2;
 class HomeController extends BaseController
 {
     public function index(){
-        $a = (new TV2())->getAll();
-        var_dump($a);die;
+        return view('main');
     }
 }
---
+++ resources/views/main.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Newsify</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="flag-in-overview">
+                            <img src="svg/flags/denmark.svg"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-10">
+                        &nbsp;
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm provider">
+                    </div>
+                    <div class="col-sm provider mr-5 ml-5">
+                    </div>
+                    <div class="col-sm provider">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
