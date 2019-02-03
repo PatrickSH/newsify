9c7d3c138502662646090a451b2813061faf7cbf
Patrick Hansen <patrick.hansen@hotmail.com>
2019-02-03 16:27:25 +0000

    Added design

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,27 +6,52 @@
         <title>Newsify</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="css/app.css">
+        <link rel="stylesheet" type="text/css" href="css/app.css?ver={{rand(1,1000)}}">
     </head>
     <body>
         <div id="app">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-2">
-                        <div class="flag-in-overview">
-                            <img src="svg/flags/denmark.svg"/>
+                <div class="country">
+                    <div class="row flag-row">
+                        <div class="col-lg-2 no-padding">
+                            <div class="flag-in-overview">
+                                <img src="svg/flags/denmark.svg"/>
+                            </div>
+                        </div>
+                        <div class="col-lg-10">
+                            &nbsp;
                         </div>
                     </div>
-                    <div class="col-lg-10">
-                        &nbsp;
+                    <div class="row">
+                        <div class="col-sm mw-33 provider">
+                            <h2>EB</h2>
+                        </div>
+                        <div class="col-sm mw-33 provider mr-5 ml-5">
+                            <h2>BT</h2>
+                        </div>
+                        <div class="col-sm mw-33 provider">
+                            <h2>TV2</h2>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm provider">
+                <div class="country">
+                    <div class="row flag-row">
+                        <div class="col-lg-2 no-padding">
+                            <div class="flag-in-overview">
+                                <img src="svg/flags/slovakia.svg"/>
+                            </div>
+                        </div>
+                        <div class="col-lg-10">
+                            &nbsp;
+                        </div>
                     </div>
-                    <div class="col-sm provider mr-5 ml-5">
-                    </div>
-                    <div class="col-sm provider">
+                    <div class="row">
+                        <div class="col-sm mw-33 provider">
+                            <h2>CAS</h2>
+                        </div>
+                        <div class="col-sm mw-33 provider mr-5 ml-5">
+                            <h2>MARKIZA</h2>
+                        </div>
                     </div>
                 </div>
             </div>
